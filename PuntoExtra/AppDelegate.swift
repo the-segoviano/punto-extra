947efa4485807cdd52f9375c167a9f4a472cd748
Luis Segoviano <the.segoviano@gmail.com>
2022-01-18 12:57:18 -0600

    Primer commit

---
+++ PuntoExtra/AppDelegate.swift
@@ -0,0 +1,46 @@
+//
+//  AppDelegate.swift
+//  PuntoExtra
+//
+//  Created by Luis Segoviano on 16/01/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let startViewController = ViewController()
+        startViewController.view.backgroundColor = .yellow
+        let navViewController = UINavigationController(rootViewController: startViewController)
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navViewController
+        window?.makeKeyAndVisible()
+        
+        
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ PuntoExtra/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PuntoExtra
+//
+//  Created by Luis Segoviano on 16/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        print(" ViewController ")
+    }
+
+}
+
